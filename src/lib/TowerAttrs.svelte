2b60b68af48c33a9df9b897860d77e0f7257383a
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-06-15 04:59:22 +0000

    sdfsfsdfsfsfsf

--- src/lib/TowerAttrs.svelte
+++ src/lib/TowerAttrs.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
   let InputElement
     function DisplayImage(): void { 
-      let FileReader = new FileReader()
-console.log(InputElement.files[0])
-   
+      let reader = new FileReader()
+let file = InputElement.files[0]
+     reader.onloadend = function(e)  {
+        let image = document.createElement("img");
+        // the result image data
+        image.src = e.target.result;
+        document.body.appendChild(image);
+     }
+     // you have to declare the file loading
+     reader.readAsDataURL(file);
   }
 </script>
 
