6315a632ed9dd211cab72321696732ce01359c06
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-06-15 04:53:47 +0000

    dgfdfgfd

--- src/lib/TowerAttrs.svelte
+++ src/lib/TowerAttrs.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
-  
+  let InputElement
+    function DisplayImage(): void { 
+      let FileReader = new FileReader()
+console.log(InputElement.files[0])
+   
+  }
 </script>
-<form on:submit={DisplayImage}>
-<input type="file" id="files" name="files" />
+
+<form on:submit|preventDefault={DisplayImage} method="post">
+<input type="file" id="files" name="files" bind:this={InputElement} />
 <button type="submit" >Submit</button>
 </form>
